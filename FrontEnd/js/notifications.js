95387a537e384e7e2dd4baac1fec8e979a38bb23
thomas_girard45 <54374885+dalton-x@users.noreply.github.com>
2023-10-13 09:31:40 +0200

    continued management of adding a new project

---
+++ FrontEnd/js/notifications.js
@@ -0,0 +1,37 @@
+// Fonction pour créer une notification
+function createNotification(message, type) {
+  // Créer l'élément de notification
+  const notification = document.createElement('div');
+  notification.className = `notification ${type}`;
+  notification.textContent = message;
+
+  // Ajouter la notification au conteneur
+  const container = document.getElementById('notification-container');
+  container.appendChild(notification);
+
+  // Supprimer la notification après quelques secondes
+  // setTimeout(() => {
+  //   closeNotification()
+  //   container.removeChild(notification);
+  // }, 3000); // Durée en millisecondes
+}
+
+// Ouvrir le modal
+function openNotifications(message, type) {
+  console.log(message, type);
+  createNotification(message, type);
+  const modal = document.getElementById('modal-notifications-container');
+  modal.style.display = 'block';
+}
+
+// Fermer le modal
+function closeNotification() {
+  const modal = document.getElementById('modal-notifications-container');
+  const container = document.getElementById('notification-container');
+  container.innerHTML = '';
+  modal.style.display = 'none';
+}
+
+// Bouton OK pour fermer le modal
+const closeButton = document.getElementById('modal-close-button');
+closeButton.addEventListener('click', closeNotification);
